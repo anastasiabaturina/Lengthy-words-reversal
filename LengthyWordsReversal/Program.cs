923e2530d921343e7f9725d4af8a200bd11e7388
Anastasia Baturina <a.baturina@sh.dunice.net>
2024-08-02 12:10:05 +0300

    ready task

---
+++ LengthyWordsReversal/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LengthyWordsReversal;
+
+public class Program
+{
+    public static void Main()
+    {
+        var result = LongWordConverter.ConvertWords("This is the test with ascii words");
+
+        Console.WriteLine(result);
+    }
+}
